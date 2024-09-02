54c961a7aad9afc4cc06d9390e7ebb1bae6dbd49
Yutong Xie <97763762+yutongxie58@users.noreply.github.com>
2024-09-01 17:58:16 -0700

    Update supabase.js: add sample url

--- src/supabase.js
+++ src/supabase.js
@@ -1,5 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = null;
+const supabaseUrl = "https://your-supabase-url";
+const supabaseKey = "your-supabase-key";
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
